e4f2924e1ea5d5c132caceeae3cf0c4e87fbf03f
Owusuansah5 <godfredowusuansah5@gmail.com>
2024-11-19 21:07:32 -0600

    Change

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w5-go-bucket-terraform"
+    bucket = "terraform"
     key    = "dev/terraform.tfstate"
     region = "us-east-2"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform"
     encrypt = true
   }
 }
